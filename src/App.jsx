e2354620761b8d5764d848f93e91f5a1d7494942
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-05-01 09:29:32 +0000

    Adding Starter Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
 
   return (
-    <div className="App text-5xl">
-      This is the App
+    <div className="App">
+      <Navbar />
+      <Home />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Cyan: "hsl(180, 66%, 49%)",
+        DarkViolet: "hsl(257, 27%, 26%)",
+        Red: "hsl(0, 87%, 67%)",
+        Gray: "hsl(0, 0%, 75%)",
+        GrayishViolet: "hsl(257, 7%, 63%)",
+        VeryDarkBlue: "hsl(255, 11%, 22%)",
+        VeryDarkViolet: "hsl(260, 8%, 14%)"
+      },
+      fontSize:{
+        base: "18px",
+      },
+      fontFamily:{
+        'poppins': ["Poppins", "sans serif"]
+      },
+    },
   },
   plugins: [],
 }
